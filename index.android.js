933b94ba69a781947e7c592210c3b15ffb06f657
15085 <huang.qi@hand-china.com>
2017-09-07 19:23:44 +0800

    [ADD] 主模块内容添加 mobile-cloud-issue#36

--- index.android.js
+++ index.android.js
@@ -1,108 +1,27 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import React, { Component } from 'react';
-import { NativeModules } from 'react-native';
-import {
-  Alert,
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  TouchableHighlight,
-  ToastAndroid
-} from 'react-native';
+'use strict';
+// import React, {
+//     Component,
+// } from 'react';
 
-export default class MobileCloud extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-  //show(){
-  //  NativeModules.NativeManager.show("hello",NativeModules.NativeManager.SHORT);
-  //}
+// import {
+//     AppRegistry,
+//     Image,
+//     TextInput,
+//     View,
+//     Platform,
+//     StyleSheet
+// } from 'react-native';
 
-  press() {
-    this.show("module");
-  }
+// import MainScreen from './MainScreen';
+//import MainScreen from './src/components/MainScreen';
 
-  pressDown() {
-    this.show("download");
-  }
+// export default class MyJd extends Component {
+//   render() {
+//     return (
+//       <MainScreen />
+//     )
+//   }
+// }
+require('./src/index.js')
 
-  show(name) {
-    NativeModules.NativeManager.openBundle(name, (type) => {
-      let title = ""
-      if (type == "update") {
-        title = "发现新版本,是否升级?"
-      } else {
-        title = "未安装应用，是否安装？"
-      }
-      Alert.alert(
-        title,
-        title,
-        [
-          {
-            text: '是',
-            onPress: () => {
-              NativeModules.NativeManager.downloadAndOpenBundle(name, (type, result) => {
-                if (type == "success") {
-                  ToastAndroid.show(result, ToastAndroid.SHORT);
-                } else {
-                  ToastAndroid.show(result, ToastAndroid.SHORT);
-                }
-              });
-            }
-          },
-          {
-            text: '否'
-          }
-        ]
-      );
-    });
-  }
-
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          安卓端主app
-        </Text>
-        <TouchableHighlight onPress={this.press.bind(this)}>
-          <Text>子模块!</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this.pressDown.bind(this)}>
-          <Text>下载新模块</Text>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-
-
-
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-
-AppRegistry.registerComponent('MobileCloud', () => MobileCloud);
+//AppRegistry.registerComponent('MyJd', () => MyJd);
---
+++ src/pages/ThirdPage.js
@@ -0,0 +1,171 @@
+'use strict';
+import React, {
+    Component,
+} from 'react';
+import { NativeModules } from 'react-native';
+import {
+    AppRegistry,
+    Image,
+    ListView,
+    StyleSheet,
+    Text,
+    View,
+    Alert,
+    TouchableWithoutFeedback,
+    RefreshControl,
+    Dimensions,
+    ToastAndroid
+} from 'react-native';
+
+var REQUEST_URL = 'http://10.211.97.242:8378/v1/bundle/getData';
+var width = Dimensions.get('window').width;
+export default class ThirdPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false,
+        };
+
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(REQUEST_URL, {
+            method: "POST",
+        })
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData),
+                    loaded: true,
+                });
+                console.log(responseData);
+            });
+    }
+
+    render() {
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+
+        return (
+            <View>
+                <Text style={styles.title}>可用模块</Text>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderIcon.bind(this)}
+                    contentContainerStyle={styles.listView}
+                />
+            </View>
+        );
+    }
+
+    renderLoadingView() {
+        return (
+            <View style={styles.containerLoading}>
+                <Text>
+                    Loading...
+                </Text>
+            </View>
+        );
+    }
+
+    _onIconClick(name, id) {
+        this.show(name, id);
+    }
+
+    show(name, id) {
+        NativeModules.NativeManager.openBundle(name, (type) => {
+            let title = ""
+            if (type == "update") {
+                title = "发现新版本,是否升级?"
+            } else {
+                title = "未安装应用，是否安装？"
+            }
+            Alert.alert(
+                title,
+                title,
+                [
+                    {
+                        text: '是',
+                        onPress: () => {
+                            NativeModules.NativeManager.downloadAndOpenBundle(name, id, (type, result) => {
+                                if (type == "success") {
+                                    //ToastAndroid.show(result, ToastAndroid.SHORT);
+                                } else {
+                                    ToastAndroid.show(result, ToastAndroid.SHORT);
+                                }
+                            });
+                        }
+                    },
+                    {
+                        text: '否'
+                    }
+                ]
+            );
+        });
+    }
+
+    renderIcon(icon) {
+        return (
+            <TouchableWithoutFeedback onPress={() => this._onIconClick(icon.name, icon.id)}>
+                <View style={styles.container}>
+                    <Image
+                        source={{ uri: icon.iconPath }}
+                        style={styles.thumbnail}
+                    />
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.year}>{icon.name}</Text>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        );
+    }
+}
+
+var styles = StyleSheet.create({
+    containerLoading: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    container: {
+        justifyContent: 'center',
+        width: width / 4,
+        height: 80,
+        alignItems: 'center',
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        textAlign: 'left',
+        fontSize: 18,
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    year: {
+        textAlign: 'center',
+        fontSize: 12,
+    },
+    thumbnail: {
+        width: 50,
+        height: 50,
+        borderRadius: 5,
+    },
+    listView: {
+        marginTop: 10,
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+});
