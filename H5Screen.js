8e2e303de48c04cba0746aba7ade435ff3d0dcb2
WUGUOKAI <guokai.wu@hand-china.com>
2017-09-28 14:45:27 +0800

    [ADD] 添加了获取到hapcloud认证页面在登录成功之后的token，之后将添加到RN中，供后端使用 mobile-cloud-issue#84

--- H5Screen.js
+++ H5Screen.js
@@ -16,15 +16,45 @@ const url = 'http://mobile-cloud.cloud.saas.hand-china.com';
 const { width, height } = Dimensions.get('window');
 
 export default class H5Screen extends Component {
+
+     //脚本注入
+    injectJS = () => {
+        const script = `alert("开始");
+                    function get(a){
+                            var arr,reg=new RegExp("(^| )"+a+"=([^;]*)(;|$)"); 
+                            if(arr=document.cookie.match(reg)) 
+                                return unescape(arr[2]); 
+                            else 
+                                return null;
+                        } 
+                        var data = null;
+                        var t = setInterval(function(){
+                            data = get('access_token');
+                            if(data != null){
+                              clearInterval(t);
+                              window.postMessage(data);
+                            }
+                          }, 2000);`;
+                          
+        if (this.webview) {
+            this.webview.injectJavaScript(script);
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
                 <WebView
+                    ref={(webview) => this.webview = webview}
+                    onLoadEnd = {this.injectJS}
                     style={{ width: width, height: height - 20, backgroundColor: 'gray' }}
                     source={{ uri: url, method: 'GET' }}
                     javaScriptEnabled={true}
-                    domStorageEnabled={true}
+                    domStorageEnabled={false}
                     scalesPageToFit={false}
+                    onMessage = {(e) => {
+                      alert(e.nativeEvent.data);
+                      console.log(e.nativeEvent.data);
+                    }}
                 />
             </View>
         )
