f26c3f88874e50668a0b5a5a67c7859ea266892d
祁煌 <huang.qi@hand-china.com>
2017-10-06 17:20:34 +0800

    [FIX] 修复获取摄像头权限在某些机型上失败的问题，扩展StackNavigation的类replace和back路由函数，获得路由栈的全控制权

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
     View,
     TouchableHighlight,
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, NavigationActions } from 'react-navigation';
 
 import MainScreen from './components/MainScreen';
 import StackNavigatorSecond from './pages/demo/StackNavigatorSecond';
@@ -57,5 +57,41 @@ export default MobileCloud = StackNavigator(
         headerMode: 'float',
     }
 );
+const prevGetStateForAction = ReactNavigation.router.getStateForAction;
+ReactNavigation.router.getStateForAction = (action, state) => {
+    if (state && action.type === 'ReplaceCurrentScreen') {
+        const routes = state.routes.slice(0, state.routes.length - 1);
+        routes.push(action);
+        return {
+            ...state,
+            routes,
+            index: routes.length - 1,
+        };
+    }
+    if (state && action.type === 'BcakToCurrentScreen') {
+        function findDateInArr(arr, propertyName, value) {
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i][propertyName] == value) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        var i = findDateInArr(state.routes, 'routeName', action.routeName);
+        if (i != -1) {
+            var routes = state.routes.slice(0, i + 1);
+            console.log('routes:-----');
+            console.log(routes);
+            console.log('action:-----');
+            console.log(action);
+        }
+        return {
+            ...state,
+            routes,
+            index: routes.length - 1,
+        }
+    }
+    return prevGetStateForAction(action, state);
+};
 
 AppRegistry.registerComponent('MobileCloud', () => MobileCloud);
